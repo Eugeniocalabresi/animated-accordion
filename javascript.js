a1e56f57a389657995238c6174baeba95fedd051
Eugenio Calabresi <92782204+Eugeniocalabresi@users.noreply.github.com>
2022-09-13 17:10:19 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,7 +17,34 @@ document.body.addEventListener('click', () => {
       Array.prototype.forEach.call(accordions, function (el) {
         el.classList.remove('cardAccordionActive');
       });
-      if (!checkClass) {
+      if (!checkClass) {document.body.addEventListener('click', () => {
+  let accordions = document.getElementsByClassName('cardAccordionActive');
+  Array.prototype.forEach.call(accordions, function (el) {
+    el.classList.remove('cardAccordionActive');
+    
+  });
+});
+
+const cats = document.querySelectorAll('.card_accordion');
+
+[...cats].forEach((cat) => {
+  cat.addEventListener('click', async function () {
+    let accordions = document.getElementsByClassName('cardAccordionActive');
+    let checkClass = false;
+    if (cat.className == 'card_accordion cardAccordionActive') {
+      checkClass = true;
+    }
+    Array.prototype.forEach.call(accordions, function (el) {
+      el.classList.remove('cardAccordionActive');
+    });
+    if (!checkClass) {
+      cat.classList.add('cardAccordionActive');
+      
+    }
+    event.stopPropagation();
+  });
+});
+
         cat.classList.add('cardAccordionActive');
       }
       event.stopPropagation();
