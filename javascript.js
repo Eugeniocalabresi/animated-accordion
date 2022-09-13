1356a244dc21bd20720b2ac442f5f998237cbab5
Eugenio Calabresi <92782204+Eugeniocalabresi@users.noreply.github.com>
2022-09-13 11:53:10 +0200

    add files
    first-commit

---
+++ javascript.js
@@ -0,0 +1,26 @@
+document.body.addEventListener('click', () => {
+    let accordions = document.getElementsByClassName('cardAccordionActive');
+    Array.prototype.forEach.call(accordions, function (el) {
+      el.classList.remove('cardAccordionActive');
+    });
+  });
+  
+  const cats = document.querySelectorAll('.card_accordion');
+  
+  [...cats].forEach((cat) => {
+    cat.addEventListener('click', async function () {
+      let accordions = document.getElementsByClassName('cardAccordionActive');
+      let checkClass = false;
+      if (cat.className == 'card_accordion cardAccordionActive') {
+        checkClass = true;
+      }
+      Array.prototype.forEach.call(accordions, function (el) {
+        el.classList.remove('cardAccordionActive');
+      });
+      if (!checkClass) {
+        cat.classList.add('cardAccordionActive');
+      }
+      event.stopPropagation();
+    });
+  });
+  
